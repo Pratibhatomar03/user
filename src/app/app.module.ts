4588c2c8335f43cb11d5f9671b94653a0f9cf443
Pratibha <pratibha.tomar13@gmail.com>
2021-09-05 21:40:58 +0200

    implemented reactive forms for different form fileds of registation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, RegistrationComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.registrationForm = formBuilder.group({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      number: new FormControl(),
+      street: new FormControl(),
+      houseNumber: new FormControl(),
+      zipCode: new FormControl(),
+      city: new FormControl(),
+      accountHolder: new FormControl(),
+      iban: new FormControl(),
+    });
   }
 
+  ngOnInit(): void {}
+  captureForm() {
+    console.log(this.registrationForm.value.iban);
+  }
 }
